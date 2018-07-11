f06733c67a6bbdd9645f603de5974e67b4cdde23
Eamon Kenny <eamokenny@gmail.com>
2018-07-11 11:47:35 +0000

    add 5th chart

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ queue()
         by_province(ndx);
         by_year(ndx);
         by_month(ndx);
+        by_region(ndx);
 
 
 
@@ -25,55 +26,46 @@ queue()
          }
          
          
-         
-         
-   
 
 
-
-
-         
-         
-         
-
- function show_year_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('year'));
-    var group = dim.group();
-    
-    dc.selectMenu("#year-selector")
-        .dimension(dim)
-        .group(group);
-}         
-
-
-function show_gender_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('gender'));
-    var group = dim.group();
-    
-    dc.selectMenu("#gender-selector")
-        .dimension(dim)
-        .group(group);
-}        
-
-
-function show_province_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('province'));
-    var group = dim.group();
-    
-    dc.selectMenu("#province-selector")
-        .dimension(dim)
-        .group(group);
-}        
-
-
-function show_county_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('county'));
-    var group = dim.group();
-    
-    dc.selectMenu("#county-selector")
-        .dimension(dim)
-        .group(group);
-}
+        function show_year_selector(ndx) {
+            var dim = ndx.dimension(dc.pluck('year'));
+            var group = dim.group();
+            
+            dc.selectMenu("#year-selector")
+                .dimension(dim)
+                .group(group);
+            }         
+        
+        
+        function show_gender_selector(ndx) {
+            var dim = ndx.dimension(dc.pluck('gender'));
+            var group = dim.group();
+            
+            dc.selectMenu("#gender-selector")
+                .dimension(dim)
+                .group(group);
+            }        
+        
+        
+        function show_province_selector(ndx) {
+            var dim = ndx.dimension(dc.pluck('province'));
+            var group = dim.group();
+            
+            dc.selectMenu("#province-selector")
+                .dimension(dim)
+                .group(group);
+            }        
+        
+        
+        function show_county_selector(ndx) {
+            var dim = ndx.dimension(dc.pluck('county'));
+            var group = dim.group();
+            
+            dc.selectMenu("#county-selector")
+                .dimension(dim)
+                .group(group);
+            }
 
         
         
@@ -119,35 +111,67 @@ function show_county_selector(ndx) {
             var name_dim = ndx.dimension(dc.pluck('year'));
             var home_goals = name_dim.group().reduceSum(dc.pluck('gpvisit'));
         
-        dc.barChart("#year_data_barchart")
-            .width(500)
-            .height(200)
-            .margins({top: 10, right: 50, bottom: 30, left: 100})
-            .dimension(name_dim)
-            .group(home_goals)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Year")
-            .yAxis().ticks(6);
-        }
-        
+            dc.barChart("#year_data_barchart")
+                .width(500)
+                .height(200)
+                .margins({top: 10, right: 50, bottom: 30, left: 100})
+                .dimension(name_dim)
+                .group(home_goals)
+                .transitionDuration(500)
+                .x(d3.scale.ordinal())
+                .xUnits(dc.units.ordinal)
+                .xAxisLabel("Year")
+                .gap(20)
+                .yAxisLabel("Number of GP visits")
+                .elasticY(true)
+                .colors(d3.scale.ordinal().range(["#ffff99"]));
+            }
+            
 
-                function by_month(ndx){
+        function by_month(ndx){
             var name_dim = ndx.dimension(dc.pluck('month'));
             var home_goals = name_dim.group().reduceSum(dc.pluck('gpvisit'));
         
-        dc.barChart("#month_data_barchart")
-            .width(500)
-            .height(200)
-            .margins({top: 10, right: 50, bottom: 30, left: 100})
-            .dimension(name_dim)
-            .group(home_goals)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Year")
-            .yAxis().ticks(6)
+            dc.barChart("#month_data_barchart")
+                .width(500)
+                .height(200)
+                .margins({top: 10, right: 50, bottom: 30, left: 100})
+                .dimension(name_dim)
+                .group(home_goals)
+                .transitionDuration(500)
+                .x(d3.scale.ordinal())
+                .xUnits(dc.units.ordinal)
+                .xAxisLabel("Year")
+                .gap(5)
+                .yAxisLabel("Number of GP visits")
+                .elasticY(true)
+                .colors(d3.scale.ordinal().range(["#bbff99"]))
+                .selectAll("text")  
+                ;
+                
+            }
             
-        }
+            
+            
+            
+    
+        
+        
+        
+        
+
+ function by_region(ndx){
+            var name_dim = ndx.dimension(dc.pluck('region'));
+            var home_goals = name_dim.group().reduceSum(dc.pluck('gpvisit'));
+
+dc.rowChart("#region_rowchart")
+  .width(550)
+  .height(250)
+  .margins({top: 20, left: 15, right: 10, bottom: 20})
+  .dimension(name_dim)
+  .group(home_goals)
+  .elasticX(true)
+  .x(d3.scale.ordinal())  
+  ;
+ }
         
